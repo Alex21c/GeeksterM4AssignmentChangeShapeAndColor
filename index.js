cfae376f15efac2dc52f1ae6a485ff0e45259f05
Abhishek kumar <aasalex21c@gmail.com>
2024-01-10 17:26:26 +0530

    initial commit !

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+let shapes=['triangle', 'circle', 'square', 'star', 'diamond', 'pentagon'];
+let shape = document.querySelector('#shape');
+let circle = document.querySelector('#circle');
+
+document.querySelector('#changeShape').addEventListener('click', function(){
+  let idx= Math.floor(Math.random() * shapes.length);
+  shape.className=shapes[idx];
+});
+
+document.querySelector('#changeColor').addEventListener('click', function(){
+  circle.style.backgroundColor= generateRandomColor();
+});
+
+function generateRandomColor(){
+  let red = Math.floor(Math.random()*256); // 0 to 255
+  let green = Math.floor(Math.random()*256);
+  let blue = Math.floor(Math.random()*256);
+  console.log(`rgb(${red}, ${green}, ${blue})`); 
+  return `rgb(${red}, ${green}, ${blue})`;
+
+}
