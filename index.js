5f132ec5b9c14db07d15d1ce3e20d73de82c98ab
Abhishek kumar <aasalex21c@gmail.com>
2024-01-11 10:53:31 +0530

    added animation effects! shpae background color is also updating now!

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ let circle = document.querySelector('#circle');
 
 document.querySelector('#changeShape').addEventListener('click', function(){
   let idx= Math.floor(Math.random() * shapes.length);
+  // making sure new generated classname is not same as current one, i.e. if current shape is circle, i want other shapes other than circle
+  while(shape.className===shapes[idx]){
+    idx= Math.floor(Math.random() * shapes.length);
+  }
+
   shape.className=shapes[idx];
 });
 
 document.querySelector('#changeColor').addEventListener('click', function(){
   circle.style.backgroundColor= generateRandomColor();
+  shape.style.backgroundColor= generateRandomColor();
 });
 
 function generateRandomColor(){
